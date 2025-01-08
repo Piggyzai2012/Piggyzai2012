4bf66fef45e2bd7b4cce248e252ecc3ebb0f8037
Piggyzai2012 <chong95@live.com>
2025-01-08 13:05:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -158,12 +158,19 @@ Income = income_mapping.get(input22)
 
 # Combine inputs into a single array for prediction
 inputs = np.array([[HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, HeartDiseaseorAttack, PhysActivity, Fruits, Veggies, HvyAlcoholConsump, AnyHealthcare, NoDocbcCost, GenHlth, MentHlth, PhysHlth, DiffWalk, Sex, Age, Education, Income]])
- 
-model_file = 'diabetes_model.pkl'
-model = joblib.load(model_file)
 
+# Load the dataset
 df = pd.read_csv('Diabetes_Data_Processed.csv')
 
+# Preprocess the data (adjust according to your dataset)
+X = df.drop('Outcome', axis=1)  # Features
+y = df['Outcome']  # Target
+
+# Split the data into training and testing sets for accuracy calculation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model_file = 'diabetes_model.pkl'
+model = joblib.load(model_file)
 
 accuracy = model.score(X_test, y_test)
 
