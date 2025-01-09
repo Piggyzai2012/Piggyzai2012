7475be3f8e8f6248e369600b5198819e49278738
Kee <chong95@live.com>
2025-01-10 03:51:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -202,4 +202,4 @@ if st.button('Predict'):
         st.write("The model predicts: **No**, Low Risk of Heart Disease.")
 
     # Calculate the accuracy on the test dataset
-    # st.write(f"Model Accuracy: **{auc_score * 100:.2f}%**")
+    st.write(f"Model Accuracy: **{auc_score * 100:.2f}%**")
