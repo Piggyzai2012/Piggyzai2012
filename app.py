fd237325d7dd2f99e86eb273df4b0f4e0b0431b2
Piggyzai2012 <chong95@live.com>
2025-01-10 01:06:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import joblib
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold
+from imblearn.over_sampling import SMOTE
 
 # Define the app layout
 st.title('Diabetes Prediction')
