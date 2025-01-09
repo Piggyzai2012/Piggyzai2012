b58293752922dbd208ba5c9cecffa13deea595e7
Kee <chong95@live.com>
2025-01-10 02:57:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold
 from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
 
 # Define the app layout
 st.title('Diabetes Prediction')
